701b3274d9a41bd97185d992352046ff7139b5aa
AchrafAtt <G134219290@taalim.ma>
2025-03-18 14:47:38 +0000

    using trait to handel response and try make v1 api login

---
+++ app/Http/Controllers/Autcontroller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ApiLoginRequest;
+use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
+
+class Autcontroller extends Controller
+{
+    use ApiResponses;
+
+    public function login(ApiLoginRequest $request){
+        //return json
+        // return response()->json([
+        //     'message' => 'Login Success'
+        // ], 200);
+
+        //*using trait
+        return $this->ok($request->get('email')); 
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Autcontroller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [Autcontroller::class,'login']);
+
